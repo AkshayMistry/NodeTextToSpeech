14167b965738e635b5841fae245d056cfd701c2c
Akshay Mistry <akshay.mistry0503@gmail.com>
2020-09-25 17:44:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+const alert = require('alert-node')
 const express = require('express')
 const gtts = require('gtts.js').gTTS
 const bodyparser = require('body-parser')
@@ -6,21 +8,29 @@ const app = express()
 
 app.use(bodyparser.json())
 app.use(bodyparser.urlencoded({ extended: true }))
-app.set('view engine','ejs')
+app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
     res.render('index')
 })
 
+
 app.post('/', (req, res) => {
     var text = req.body.text
-    const speech = new gtts(text)
-    speech.save("output.mp3")
-        .then(function () {
-          res.download("output.mp3")
-        }).catch(function (err) {
-        
-    })
+    
+    
+    if (!text) {
+        alert('Please enter text.')
+    } else {
+        const speech = new gtts(text)
+        var filename = "Audio" + Date.now() + ".mp3"
+        speech.save(filename)
+            .then(function () {
+                res.download(filename)
+            }).catch(function (err) {
+
+            })
+    }
 })
 
 app.listen(PORT, function () {
